90cfc3a5627ce724fd515125c3dfd4ad9c7a9020
Malik4647 <malikumarmahmood@outlook.com>
2023-02-04 19:35:23 +0500

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errormiddleware");
+const connectDB = require("./config/db");
 const port = process.env.port || 5000;
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
